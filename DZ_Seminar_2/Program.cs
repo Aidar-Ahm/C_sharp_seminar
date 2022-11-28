a0798d4903c23a6a034b0ded878df46dcd79b081
Aidar-Ahm <aidar.akhmedzyanov@gmail.com>
2022-11-28 10:22:00 +0500

    Дз семинар 2

--- DZ_Seminar_2/Program.cs
+++ DZ_Seminar_2/Program.cs
@@ -1,13 +1,13 @@
 ﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе 
 //показывает вторую цифру этого числа. Через строку решать нельзя.
 
-// Console.WriteLine("Введите трехзначное число");
-// int num = Convert.ToInt32(Console.ReadLine());
-// if (num<1000 && num>99)
-// {
-//     Console.WriteLine($"Вторая цифра {num/10%10}");
-// }
-// else Console.WriteLine("Введенно неверное число");
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num<1000 && num>99)
+{
+    Console.WriteLine($"Вторая цифра {num/10%10}");
+}
+else Console.WriteLine("Введенно неверное число");
 
 
 //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
@@ -40,22 +40,50 @@
 //При этом вместо чисел, кратных трем, программа должна выводить слово Fizz, а вместо чисел, 
 //кратных пяти — слово Buzz. Если число кратно пятнадцати, то программа должна выводить слово FizzBuzz. 
 
+//СПОСОБ 1 Время выполнения 20 милисекунд.
+// using System.Diagnostics;
+// var sw = new Stopwatch();
+// sw.Start(); // запуск таймера
+
+// string fizzbuzz (int i)
+// {
+//     string res;
+//     if (i % 15 == 0) res = " FizzBuzz";
+//     else if (i % 5 == 0) res = " Buzz";
+//     else if (i % 3 == 0) res = " Fizz";
+//     else res = ($" {i}");
+//     return res;
+// }
+// for (int i = 1; i < 101; i++)
+// {
+//     fizzbuzz(i);
+//     Console.Write(fizzbuzz(i));
+// }
+
+// sw.Stop();// конец таймера
+// Console.WriteLine($"Время выполнения {sw.ElapsedMilliseconds}");
+
+//СПОСОБ 2 Время выполнения 14 милисекунд
+// using System.Diagnostics;
+// var sw = new Stopwatch();
+// sw.Start(); // запуск таймера
+
 // for (int i = 1; i < 101; i++)
 // {
 //     if (i % 15 == 0) Console.Write(" FizzBuzz");
 //     else if (i % 5 == 0) Console.Write(" Buzz");
 //     else if (i % 3 == 0) Console.Write(" Fizz");
 //     else Console.Write($" {i}");
-
 // }
-
+// sw.Stop();// конец таймера
+// Console.WriteLine($"Время выполнения {sw.ElapsedMilliseconds}");
 
 //Напишите программу, считывающую с пользовательского ввода целое число n (неотрицательное), 
 //выводящее это число в консоль вместе с правильным образом изменённым словом "программист"
 
-Console.WriteLine("Введите количество программистов");
-int count = Convert.ToInt32(Console.ReadLine());
-if (count == 1 || count % 10 == 1) Console.WriteLine($"{count} программист");
-else if (count > 1 && count < 5 || count % 10 > 1 && count % 10 < 5) Console.WriteLine($"{count} программиста");
-else Console.WriteLine($"{count} программистов");
+// Console.WriteLine("Введите количество программистов");
+// int count = Convert.ToInt32(Console.ReadLine());
+// if (count == 1 || count % 10 == 1) Console.WriteLine($"{count} программист");
+// else if (count > 1 && count < 5 || count % 10 > 1 && count % 10 < 5) Console.WriteLine($"{count} программиста");
+// else Console.WriteLine($"{count} программистов");
 
